585a5ef377650ae85ee6c413d62ee9c985b8b40a
physics3D <kroener.tobias@t-online.de>
2022-01-06 11:36:07 +0100

    changes

--- src/consts.rs
+++ src/consts.rs
@@ -7,13 +7,7 @@ pub const KEY_PAIR_LENGTH: usize = 2048;
 
 // lazily create the needed start for a block hash: a null byte DIFFICULTY times
 lazy_static! {
-    pub static ref NEEDED_HASH_START: Vec<u8> = {
-        let mut vec = vec![];
-        for _ in 0..DIFFICULTY {
-            vec.push(0u8);
-        }
-        vec
-    };
+    pub static ref NEEDED_HASH_START: Vec<u8> = vec![0; DIFFICULTY as usize];
 }
 
 pub const BUFFER_SIZE: usize = 4096;
